cea6ee214f5e7579bcb51ec39e9ec095b22a127c
Brian Jing <brianjing1@gmail.com>
2025-10-31 02:26:15 -0700

    fixing circle

--- projects/projects.js
+++ projects/projects.js
@@ -15,3 +15,39 @@ const titleElement = document.querySelector('.projects-title');
 if (titleElement) {
     titleElement.textContent = `${projects.length} Projects `;
 }
+
+
+// D3 Stuff:
+
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let arc = arcGenerator({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+});
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
+
+let data = [1, 2];
+let total = 0;
+for (let d of data) {
+  total += d;
+}
+
+
+let angle = 0;
+let arcData = [];
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach((arc) => {
+    // TODO, fill in step for appending path to svg using D3
+});
